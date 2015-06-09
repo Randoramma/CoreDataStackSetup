f9e4ab74a5bad7ba2ea59a194032f727681837b3
Matt Morton <mmorton@torpedogroup.com>
2015-06-09 15:01:41 +0100

    Renaming completion handler in Swift
    No need to have two separate typedef’s for handler.

--- CoreDataStackSetup/DatabaseManager.swift
+++ CoreDataStackSetup/DatabaseManager.swift
@@ -1,16 +1,15 @@
 import UIKit
 import CoreData
 
-typealias DatabaseManagerStackSetupCompletionHandler = (result: Bool, failureError: NSError?) -> Void
-typealias DatabaseManagerSaveCompletionHandler = (result: Bool, failureError: NSError?) -> Void
+typealias DatabaseManagerCompletionHandler = (result: Bool, failureError: NSError?) -> Void
 
 class DatabaseManager {
 
   private(set) var mainThreadManagedObjectContext: NSManagedObjectContext
   private var saveManagedObjectContext: NSManagedObjectContext
   
-  init (completion: DatabaseManagerStackSetupCompletionHandler) {
     let modelURL = NSBundle.mainBundle().URLForResource("MyDataModel", withExtension: "momd")!
+  init (completion: DatabaseManagerCompletionHandler) {
     let mom = NSManagedObjectModel(contentsOfURL: modelURL)!
 
     var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: mom)
@@ -49,7 +48,7 @@ class DatabaseManager {
     })
   }
   
-  func saveDataWithCompletionHandler(completion: DatabaseManagerSaveCompletionHandler) {
+  func saveDataWithCompletionHandler(completion: DatabaseManagerCompletionHandler) {
     if (!NSThread.isMainThread()) {
       dispatch_sync(dispatch_get_main_queue(), { () -> Void in
         self.saveDataWithCompletionHandler(completion)
